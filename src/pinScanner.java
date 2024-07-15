2cdf768f96e4cbd4505912324bb0d4409c4eb95b
Eno1bas <E.Ibas@protonmail.com>
2024-07-15 17:06:29 +0100

    Pin Scanner

---
+++ src/pinScanner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class pinScanner {
+    public static void main (String[] args){
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter your Pin");
+      String Pin = scan.nextLine();
+    }
+}
